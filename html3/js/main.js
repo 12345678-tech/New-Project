458360ab12de6b2aa38ee39b54d3c04b643a7327
AyaanJ <66999423+12345678-tech@users.noreply.github.com>
2022-06-09 16:33:58 +0530

    Add files via upload
    mocktest 3

---
+++ html3/js/main.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function(){	
+
+	$('.slider-carousel').owlCarousel({
+		items:1,
+		loop:true,
+		autoplay:true,
+		slideSpeed : 200,
+		paginationSpeed : 800,
+    rewindSpeed : 1000,
+		slideSpeed : 200,
+		dots:true
+	});
+		$('.famous-carousel').owlCarousel({
+		items:6,
+		loop:true,
+		margin:24,
+		autoplay:true,
+		slideSpeed : 200,
+		paginationSpeed : 800,
+    rewindSpeed : 1000,
+    nav:true,
+        navText : ['<i class=""></i>','<i class=""></i>'],
+		slideSpeed : 200,
+		dots:false,
+		responsive:{
+			1080:{items:6},
+			768:{items:2},
+			600:{items:2},
+			425:{items:2},
+			360:{items:2},
+			30:{items:2}
+			
+		}
+
+	});
+		
+ AOS.init({easing: 'ease-in-out-sine',once:true});
+
+});
+
+$(window).scroll(function() {
+			if ($(this).scrollTop() >= 100) {        // If page is scrolled more than 50px
+				$('#return-to-top').fadeIn(200);    // Fade in the arrow
+			} else {
+				$('#return-to-top').fadeOut(200);   // Else fade out the arrow
+			}
+	   });
+		$('#return-to-top').click(function() {      // When arrow is clicked
+			$('body,html').animate({
+				scrollTop : 0                       // Scroll to top of body
+			}, 500);
+		});
